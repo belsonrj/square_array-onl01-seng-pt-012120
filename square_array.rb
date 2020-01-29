2424caf00109e9be11b0a3b19c85fc59a02bd0cd
Rick Belson <belsonrj@aol.com>
2020-01-29 04:40:42 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ numbers = [1,2,3]
 
 def square_array(array)
   new_array = []
-  array.each { |i| arr << i ** 2 }
-    
-    end
+  array.each { |number| arr << number ** 2 }
+  new_array  
+  end
 end
 
